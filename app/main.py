fc1c6a5ce2b046fff353c32bbeaf76c0b99deb02
Laura del Pino Díaz <lauradelpinodiaz94@gmail.com>
2026-04-16 09:00:42 +0100

    - Global exception handler

--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
-﻿from fastapi import FastAPI, HTTPException,Depends, status, BackgroundTasks
+﻿from fastapi import FastAPI, HTTPException,Depends, status,  Request, BackgroundTasks
+from fastapi.responses import JSONResponse
 from contextlib import asynccontextmanager
 from sqlmodel import Session
 import model as m
 import controller as c
 import db
 
+# region App configuration
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     db.create_db_and_tables()
@@ -14,6 +16,19 @@ app = FastAPI(title="Notification Service (Technical Test)", lifespan=lifespan)
 # we don't have to do anything with the port since it's already config in the Dockerfile
 
 
+@app.exception_handler(Exception)
+async def global_exception_handler(request: Request, exc: Exception):
+    return JSONResponse(
+        status_code=500,
+        content={
+            "error": True,
+            "type": "internal_server_error",
+            "detail": "Unexpected error",
+            "path": str(request.url.path),
+        },
+    )
+
+# endregion
 
 @app.post(
     path="/v1/requests",
